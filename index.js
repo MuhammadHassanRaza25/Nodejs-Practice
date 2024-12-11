9bd867a8d7c302e0373b01c7886bafb4771352fa
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-12-12 00:08:01 +0500

    nodejs practice

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const PORT = 4000;
 app.get('/', (req, res)=>{
   console.log("req==>", req);
   res.send('Muhammad Hassan Raza > MERN Stack Developer 🚀📊📱')
-})   
-// summary
-// jab bhi request ati hai to callback milta hai or usme 2chezain milti hain. 1.request 2.response
-// response:res dena zarori hai agar ham response:res nahi send karenge to localhost par reload hota rahega.
-
+})  
+ 
 // server localhost pe run karne ke liye
 app.listen(PORT,()=>{
     console.log('Server is running on PORT' + PORT);
 })
+
+
+//SUMMARY OF CODE
+
+// app.get summary
+// jab bhi request ati hai to callback milta hai or usme 2chezain milti hain. 1.request 2.response
+// response:res dena zarori hai agar ham response:res nahi send karenge to localhost par reload hota rahega.
 
 // koi bhi file run karne ke liye. in cmd: node fileName.js
 // agar main koi change karon to mujhy bar bar server nahi chalana pare iske liye
